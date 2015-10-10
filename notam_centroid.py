0b94ae358b84d47bb09b78bb3097d63364e2b158
Antonio Locandro <antoniolocandro@hotmail.com>
2015-10-09 19:17:53 -0600

    web browser map
    adds the info to a web browser

--- notam_centroid.py
+++ notam_centroid.py
@@ -1,33 +1,38 @@
 __author__ = 'Antonio Locandro'
+
 '''
 Script only works for Central America FIR (FIR MHTG) due to hard coded values
 '''
 
 # import required modules
+from geographiclib import geodesic
 import os
 import csv
 import Tkinter
 root = Tkinter.Tk()
 root.withdraw()
 from tkFileDialog import *
-
 from shapely.geometry import Polygon
 from geopy.distance import vincenty
 from math import ceil
 from shapely.wkt import dumps, loads
-
+import webbrowser
 
 csv_file = askopenfilename(title="csv for radius of influence calculation",initialfile='*.csv')
 
 cvr = "Polygon(("
+notam_line = []
 
 with open(csv_file, 'rb') as csvfile:
     csv_read = csv.reader(csvfile, delimiter=',', quotechar='|')
     next(csv_read, None)
     for row in csv_read:
         cvr+=""+row[9]+" "+row[8]+","
-
+        notam_line.append((float(row[8]),float(row[9])))
 pl = loads (cvr.rstrip(",")+"))")
+#notam_line = notam_line.rstrip(",")
+
+#print notam_line
 
 #debug print
 #print pl
@@ -38,6 +43,9 @@ vl = pl.centroid
 
 x = vl.x
 y = vl.y
+
+#debug print
+#print x,y
 
 
 def dd_dmsX(dd):
@@ -66,3 +74,39 @@ for v in list(pl.exterior.coords):
 radius = int(ceil(max(rl)))
 
 print 'NOTAM Coordinates and Radius\n%s%s%03d'%(dd_dmsY(y)[0],dd_dmsX(x)[0],radius)
+
+
+'''
+circle calculation
+'''
+cnotam = []
+def circle(lat,lon):
+
+    for i in range(0,360,1):
+        result=geodesic.Geodesic.WGS84.Direct(lat, lon, i, radius*1852)
+        lat2, lon2 = result["lat2"], result["lon2"]
+        cnotam.append((lat2,lon2))
+        #debug print
+        #print lat2,lon2
+
+circle(y2,x2)
+#print cnotam
+
+
+'''
+visualization
+'''
+
+import folium
+map_osm = folium.Map(location=[y, x],zoom_start=12)
+map_osm.circle_marker(location=[y, x], radius=100,
+                  popup='Calculated Centroid', line_color='#3186cc',
+                  fill_color='#3186cc', fill_opacity=0.2)
+map_osm.simple_marker ([y2, x2],popup='NOTAM Centroid and Radius of Influence\n%s%s%03d'%(dd_dmsY(y)[0],dd_dmsX(x)[0],radius))
+#print notam_line
+map_osm.line(notam_line, line_color='#FF0000', line_weight=3)
+map_osm.line(cnotam, line_color='#3186cc', line_weight=10)
+map_osm.create_map(path='C:\\Users\\Antonio\\Desktop\\notam_radius.html')
+
+url = "C:\\Users\\Antonio\\Desktop\\notam_radius.html"
+webbrowser.open(url,new=1)
