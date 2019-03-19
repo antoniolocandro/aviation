621b7db2f7e795e22dfc2af13c95a400dec0f224
Antonio Locandro <antoniolocandro@hotmail.com>
2019-03-19 19:15:37 +0000

    update

--- flightaware_scraper.py
+++ flightaware_scraper.py
@@ -12,7 +12,7 @@ def replaceMultiple (mainString,toBeReplaces,newString):
     return mainString
 
 
-AD = 'MGPB'
+AD = 'MHLM'
 #url= 'http://flightaware.com/live/flight/SWA827/history/20151213/2020Z/KHOU/MZBZ/tracklog'
 
 url = raw_input('enter url  ')
@@ -28,19 +28,19 @@ flight_date = soup.title.text.replace(u'\u2708',",").split(',')[2].strip(' ')
 #print flight_date
 #flight_date = 'nil'
 flight_destination = soup.title.text.replace(u'\u2708',",").split(',')[3].strip(' ').split('/')[1].strip(' ')
-#flight_destination = 'nil'
+#flight_destination = 'MGPB'
 #print flight_destination
 
 #^smallrow\d
 table = soup.find_all("tr", { "class" : re.compile(r"^smallrow1|smallrow2")})
-print table
+#print table
  #with open('eggs.csv', 'rb') as csvfile:
   #  spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 bpath ="C://erase/antopy"
 #bname = flight_destination+'_'+flight_date+'_'+flight_airline
 bname = flight_date+'_'+flight_airline
 
-out = bpath+'/'+AD+'/csv/2018/ref/'+bname +'.csv'
+out = bpath+'/'+AD+'/csv/2019/ref/'+bname +'.csv'
 #out = bpath+'/csv/'+bname +'.csv'
 
 out_file = open(out,'wb')
@@ -59,7 +59,9 @@ for row in table:
 
             j = '%i,%s,%f,%f,%s,%s,%s,%s'%(count,cells[0].text.encode('utf-8'),float(cells[1].text.encode('utf-8')[:7]),float(cells[2].text.encode('utf-8')[:8]),cells[4].text.encode('utf-8'),(cells[5].text.encode('utf-8')),(cells[6].text.encode('utf-8')),float(cells[7].text.encode('utf-8').replace(',','')[:len(cells[7].text.encode('utf-8'))/2]))
 
-            #print j
+
+            print j
+
             out_data.append(j.split(','))
             count +=1
         elif len(cells) ==9 and cells[7].text.encode('utf-8') is not '':
@@ -68,14 +70,19 @@ for row in table:
             #print 2,cells[2].text.encode('utf-8')[:8]
             #print 3,replaceMultiple(cells[3].text.encode('utf-8'),['&searr;','rarr;','nerr;','&deg;'],"")
             #print 4,cells[4].text.encode('utf-8')
-            #print 6,float(cells[6].text.encode('utf-8').replace(",","."))*1000
+            #print 6,cells[6].text.encode('utf-8')
+            #print 6,cells[6].text.encode('utf-8').replace(",", " ").split()[-1:][-4:]
             #print 7,cells[7].text.encode('utf-8')
             #print ''
 
             #j = '%i,%s,%f,%f,%s,%s,%s,%s'%(count,cells[0].text.encode('utf-8'),float(cells[1].text.encode('utf-8')[:7]),float(cells[2].text.encode('utf-8')[:8]),cells[4].text.encode('utf-8'),(cells[5].text.encode('utf-8')),(cells[6].text.encode('utf-8')),float(cells[7].text.encode('utf-8').replace(',','')[:len(cells[7].text.encode('utf-8'))/2]))
-            j = '%i,%s,%f,%f,%s,%s,%s,%s,%s' % (count, cells[0].text.encode('utf-8'), float(cells[1].text.encode('utf-8')[:7]),float(cells[2].text.encode('utf-8')[:8]),replaceMultiple(cells[3].text.encode('utf-8'),['\xc2','\xe2','\xC3','\xC2','&searr;','&rarr;','&nearr;','&swarr','&nwarr','&deg;'],""), cells[4].text.encode('utf-8'), (cells[5].text.encode('utf-8')),float(cells[6].text.encode('utf-8').replace(",","."))*1000,replaceMultiple(cells[7].text.encode('utf-8'),[',','\xc2','\xe2','\xC3','\xC2'],""))
+            #j = '%i,%s,%f,%f,%s,%s,%s,%s,%s' % (count, cells[0].text.encode('utf-8'), float(cells[1].text.encode('utf-8')[:7]),float(cells[2].text.encode('utf-8')[:8]),replaceMultiple(cells[3].text.encode('utf-8'),['\xc2','\xe2','\xC3','\xC2','&searr;','&rarr;','&nearr;','&swarr','&nwarr','&deg;'],""), cells[4].text.encode('utf-8'), (cells[5].text.encode('utf-8')),float(cells[6].text.encode('utf-8').replace(",","."))*1000,replaceMultiple(cells[7].text.encode('utf-8'),[',','\xc2','\xe2','\xC3','\xC2'],""))
+            #j = '%i,%s,%f,%f,%s,%s,%s,%s,%s' % (count, cells[0].text.encode('utf-8'), float(cells[1].text.encode('utf-8')[:7]),   float(cells[2].text.encode('utf-8')[:8]), replaceMultiple(cells[3].text.encode('utf-8'),['\xc2', '\xe2', '\xC3', '\xC2', '&searr;','&rarr;', '&nearr;', '&swarr', '&nwarr','&deg;'], ""), cells[4].text.encode('utf-8'),(cells[5].text.encode('utf-8')), cells[6].text.encode('utf-8').replace(",", "") * 1000, replaceMultiple(cells[7].text.encode('utf-8'), [',', '\xc2', '\xe2', '\xC3', '\xC2',';'], ""))
+            j = '%i,%s,%f,%f,%s,%s,%s,%s,%s' % (count, cells[0].text.encode('utf-8')[:-7], float(cells[1].text.encode('utf-8')[:7]),float(cells[2].text.encode('utf-8')[:8]), replaceMultiple(cells[3].text.encode('ascii',errors='ignore'),[';','\xef','\xc2', '\xe2', '\xC3', '\xC2','&searr', '&searr;','&rarr;', '&nearr;', '&swarr', '&nwarr','&deg;'], ""), cells[4].text.encode('utf-8'),(cells[5].text.encode('utf-8')), str(cells[6].text.encode('ascii',errors='ignore')).replace(',','')[:int(len((str(cells[6].text.encode('ascii',errors='ignore')).replace(',', '')))*.5)], replaceMultiple(cells[7].text.encode('ascii',errors='ignore'), ['\xef',',', '\xc2', '\xe2', '\xC3', '\xC2',';'], ""))
+            print j
 
-            #print j
+
+
             out_data.append(j.split(','))
             count +=1
         else:
